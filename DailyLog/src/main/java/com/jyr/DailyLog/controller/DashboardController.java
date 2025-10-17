8b2b4db289938fc3f86565055851bb45022875aa
camellia-ver <jakahi435@gmail.com>
2025-10-17 15:49:04 +0900

    (추가) 대시보드에 일기작성 여부 및 감정 표시 추가

--- DailyLog/src/main/java/com/jyr/DailyLog/controller/DashboardController.java
+++ DailyLog/src/main/java/com/jyr/DailyLog/controller/DashboardController.java
@@ -1,12 +1,38 @@
 package com.jyr.DailyLog.controller;
 
+import com.jyr.DailyLog.domain.Diary;
+import com.jyr.DailyLog.dto.DiaryResponseDto;
+import com.jyr.DailyLog.service.DiaryService;
+import com.jyr.DailyLog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+
 @Controller
+@RequiredArgsConstructor
 public class DashboardController {
+    private final DiaryService diaryService;
+    private final UserService userService;
+
     @GetMapping("/")
-    public String dashboard(){
+    public String dashboard(
+            Model model,
+            @AuthenticationPrincipal UserDetails userDetails){
+        LocalDate today = LocalDate.now();
+        Long userId = userService.findUser(userDetails.getUsername()).getId();
+
+        boolean isTodayDiary = diaryService.isTodayDiary(userId, today);
+        model.addAttribute("isTodayDiary",isTodayDiary);
+
+        DiaryResponseDto todayDiary = diaryService.findSavedDiary(userId, today);
+        model.addAttribute("todayDiary", todayDiary);
+
         return "index";
     }
 }
